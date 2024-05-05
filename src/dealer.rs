9cb3ba68059572a5d80d886e746619bb5bad3f0b
Rudolf Hrytseniak <rudolf.gritsenyak@gmail.com>
2024-05-05 18:04:30 +0200

    feat: add reraise mechanism support, increase dealer struct readability

--- src/dealer.rs
+++ src/dealer.rs
@@ -1,6 +1,6 @@
-use std::collections::{BTreeMap, HashMap};
 use pokereval_cactus::card::Card as PCard;
 use pokereval_cactus::evaluator;
+use std::collections::{BTreeMap, HashMap};
 
 use crate::{
     card::CardDeck,
@@ -71,6 +71,7 @@ struct GameState {
     bank: i32,
     big_blind: i32,
     raise_amount: i32,
+    raiser_index: Option<i32>,
 }
 
 impl GameState {
@@ -92,9 +93,10 @@ impl GameState {
                 cards: Vec::new(),
             },
             bank: 0,
-            // from settings
+            // define big blind from game settings
             big_blind: 10,
             raise_amount: 0,
+            raiser_index: None,
         }
     }
 }
@@ -296,14 +298,69 @@ impl Dealer {
         win_result
     }
 
-    pub fn check_winner(&self) -> Option<&Player> {
-        let mut valid_players = self.player_state.players.iter().filter(|p| {
+    pub fn can_determine_winner(&self) -> Option<bool> {
+        let mut remaining_players = self.player_state.players.iter().filter(|p| {
             p.action.is_some() && p.action.as_ref().unwrap().action_type != ActionType::Fold.into()
         });
-        match (valid_players.next(), valid_players.next()) {
-            (Some(player), None) => Some(player),
+
+        match (remaining_players.next(), remaining_players.next()) {
+            (Some(_), None) => Some(true),
             _ => None,
         }
+    }
+
+    fn update_next_player_index(&mut self, reset: bool) {
+        if reset {
+            self.game_state.next_player_index = 0;
+        } else {
+            self.icrement_player_index();
+        }
+    }
+
+    // TODO: refactor due to kinda bad utilitarity
+    fn icrement_player_index(&mut self) {
+        self.game_state.next_player_index =
+            (self.game_state.next_player_index + 1) % self.player_state.players.len() as i32;
+    }
+
+    fn deal_new_street(&mut self, street_status: i32) {
+        self.game_state.street.street_status = street_status;
+        if street_status == StreetStatus::Flop as i32 {
+            // maybe not needed, TODO: refactor
+            self.game_state.street.cards.clear();
+
+            self.game_state
+                .street
+                .cards
+                .push(self.deck_state.deck.cards.pop_front().unwrap());
+            self.game_state
+                .street
+                .cards
+                .push(self.deck_state.deck.cards.pop_front().unwrap());
+            self.game_state
+                .street
+                .cards
+                .push(self.deck_state.deck.cards.pop_front().unwrap());
+        } else {
+            self.game_state
+                .street
+                .cards
+                .push(self.deck_state.deck.cards.pop_front().unwrap());
+        }
+    }
+
+    fn is_last_turn(&mut self) -> bool {
+        let mut is_last_player_turn = self.game_state.next_player_index == 0;
+
+        // TODO: change check to boolean flag
+        if self.has_active_raiser() {
+            is_last_player_turn = false;
+        }
+
+        is_last_player_turn
+    }
+    fn has_active_raiser(&self) -> bool {
+        self.game_state.raiser_index.is_some()
     }
 
     pub fn update_game_state(&mut self, payload: PlayerPayload) -> ClientGameState {
@@ -316,17 +373,19 @@ impl Dealer {
             .players
             .iter()
             .any(|p| { p.user_id == payload.player_id }));
+
         // TODO: Reset player action on new game loop
 
         match ActionType::try_from(payload.action.as_ref().unwrap().action_type).unwrap() {
             ActionType::Fold => {
                 self.process_fold_action(payload.player_id);
-                let result = self.check_winner();
-                if result.is_some() == true {
+                let result = self.can_determine_winner();
+                if result.is_some() && result.unwrap() == true {
                     // TODO: set_winner
-                    let latest_winners = vec![result.unwrap().clone()];
+                    let winners = self.calculate_winner();
+                    // in future: add checks when players are elimanated;
                     let mut state = self.next_game_loop();
-                    state.latest_winners = latest_winners;
+                    state.latest_winners = winners;
                     return state;
                 }
             }
@@ -334,57 +393,33 @@ impl Dealer {
                 self.process_call_action(payload.player_id, payload.action.unwrap().bet);
             }
             ActionType::Raise => {
-                self.process_raise_action(payload.player_id, payload.action.unwrap().bet)
+                self.process_raise_action(payload.player_id, payload.action.unwrap().bet);
             }
             ActionType::Empty => println!("Empty"),
         }
-        // TODO: add functionality in case of player folded or sit outed etc.
-        let max_index = self.player_state.players.len() - 1;
-        let is_last_player_turn = self.game_state.next_player_index as usize == max_index;
 
-        if is_last_player_turn {
-            let next_street = self.game_state.street.street_status + 1;
+        self.update_next_player_index(false);
 
-            if next_street > StreetStatus::River as i32 {
+        if self.has_active_raiser()
+            && self.game_state.next_player_index == self.game_state.raiser_index.unwrap()
+        {
+            self.game_state.raiser_index = None;
+            self.update_next_player_index(true);
+        }
+
+        if self.is_last_turn() {
+            let curr_street = self.game_state.street.street_status;
+
+            if curr_street == StreetStatus::River as i32 {
                 let winners: Vec<Player> = self.calculate_winner();
                 let mut state = self.next_game_loop();
                 // TODO: set_winner
                 state.latest_winners = winners;
                 return state;
             }
-            // TODO: move_button + increase next player index;
-            // TODO: refactor to update state using interface;
-            // Implement Searching next player id depending on Raise and Folds Actions
-            self.game_state.next_player_index = 0;
-            self.game_state.street.street_status = next_street;
-            if next_street == StreetStatus::Flop as i32 {
-                // maybe not needed, TODO: refactor
-                self.game_state.street.cards.clear();
-
-                self.game_state
-                    .street
-                    .cards
-                    .push(self.deck_state.deck.cards.pop_front().unwrap());
-                self.game_state
-                    .street
-                    .cards
-                    .push(self.deck_state.deck.cards.pop_front().unwrap());
-                self.game_state
-                    .street
-                    .cards
-                    .push(self.deck_state.deck.cards.pop_front().unwrap());
-            } else {
-                self.game_state
-                    .street
-                    .cards
-                    .push(self.deck_state.deck.cards.pop_front().unwrap());
-            }
-        } else {
-            // Implement Searching next player id depending on Raise and Folds Actions
-            self.game_state.next_player_index += 1;
+            self.update_next_player_index(true);
+            self.deal_new_street(curr_street + 1);
         }
-
-        // TODO: use referenced structure for memory optimization
 
         self.get_client_game_state()
     }
@@ -428,7 +463,7 @@ impl Dealer {
             println!("Player does not have enough points!");
             return;
         }
-
+        // winners contains non-updated field bet_in_current_seed
         player.bet_in_current_seed += bet_amount;
         player.bank -= bet_amount;
         self.game_state.last_action = PlayerAction {
@@ -443,50 +478,54 @@ impl Dealer {
     }
 
     fn process_raise_action(&mut self, player_id: i32, bet_amount: i32) {
-        let player = self
+        if let Some(index) = self
             .player_state
             .players
-            .iter_mut()
-            .find(|p| p.user_id == player_id)
-            .expect("user not found");
-        // TODO: implement raise rules validation
+            .iter()
+            .position(|p| p.user_id == player_id)
+        {
+            self.game_state.raiser_index = Some(index as i32);
 
-        if player.action.as_ref().unwrap().action_type == ActionType::Fold.into() {
-            println!("Player has folded already and cannot raise");
-            return;
-        }
+            let player: &mut Player = &mut self.player_state.players[index];
+            if player.action.as_ref().unwrap().action_type == ActionType::Fold.into() {
+                println!("Player has folded already and cannot raise");
+                return;
+            }
 
-        if player.bank < bet_amount {
-            println!("Player does not have enough points!");
-            return;
-        }
+            if player.bank < bet_amount {
+                println!("Player does not have enough points!");
+                return;
+            }
 
-        let valid_min_raise = if self.game_state.raise_amount == 0 {
-            self.game_state.big_blind * 2
+            let valid_min_raise = if self.game_state.raise_amount == 0 {
+                self.game_state.big_blind * 2
+            } else {
+                self.game_state.last_action.bet + self.game_state.raise_amount
+            };
+
+            if bet_amount < valid_min_raise {
+                println!("Player raise amount is not valid");
+                return;
+            }
+
+            player.bet_in_current_seed += bet_amount;
+            player.bank -= bet_amount;
+            self.game_state.bank += bet_amount;
+            self.game_state.raise_amount = if self.game_state.raise_amount == 0 {
+                bet_amount - self.game_state.big_blind
+            } else {
+                bet_amount - self.game_state.raise_amount
+            };
+            self.game_state.last_action = PlayerAction {
+                action_type: ActionType::Raise.into(),
+                bet: bet_amount,
+            };
+            player.action = Some(PlayerAction {
+                action_type: ActionType::Raise.into(),
+                bet: bet_amount,
+            });
         } else {
-            self.game_state.last_action.bet + self.game_state.raise_amount
-        };
-
-        if bet_amount < valid_min_raise {
-            println!("Player raise amount is not valid");
-            return;
+            println!("the player with the given ID is not found");
         }
-
-        player.bet_in_current_seed += bet_amount;
-        player.bank -= bet_amount;
-        self.game_state.bank += bet_amount;
-        self.game_state.raise_amount = if self.game_state.raise_amount == 0 {
-            bet_amount - self.game_state.big_blind
-        } else {
-            bet_amount - self.game_state.raise_amount
-        };
-        self.game_state.last_action = PlayerAction {
-            action_type: ActionType::Raise.into(),
-            bet: bet_amount,
-        };
-        player.action = Some(PlayerAction {
-            action_type: ActionType::Raise.into(),
-            bet: bet_amount,
-        });
     }
 }
