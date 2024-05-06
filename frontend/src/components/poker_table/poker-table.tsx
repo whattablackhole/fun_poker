8d6726212648b31b8886b643a8b808f4d237ce07
Rudolf Hrytseniak <rudolf.gritsenyak@gmail.com>
2024-05-06 07:21:20 +0200

    feat: implement chips rendering functions

--- frontend/src/components/poker_table/poker-table.tsx
+++ frontend/src/components/poker_table/poker-table.tsx
@@ -10,13 +10,14 @@ function PokerTable(init_state: ClientState) {
     const betInputRef: RefObject<HTMLInputElement> = useRef(null);
     const [state, setState] = useState(init_state);
     let canvas_width = 1000;
-    let canvas_height = 1000;
+    let canvas_height = 700;
     const centerX = canvas_width / 2;
     const centerY = canvas_height / 2;
-    const radius = 250;
+    const radius = 150;
     const scaleX = 1.7;
     const scaleY = 1.2;
-
+    let scaledX = centerX / scaleX;
+    let scaledY = centerY / scaleY;
     useEffect(() => {
         const canvas: HTMLCanvasElement = canvasRef.current as HTMLCanvasElement;
         // read about init_state param and useState default arg, is it in sync?
@@ -34,13 +35,19 @@ function PokerTable(init_state: ClientState) {
                 canvas.width = canvas_width;
                 canvas.height = canvas_height;
 
-
                 ctx.scale(scaleX, scaleY);
                 ctx.beginPath();
-                ctx.arc(centerX / scaleX, centerY / scaleY, radius, 0, Math.PI * 2);
+                ctx.arc(scaledX, scaledY, radius, 0, Math.PI * 2);
                 ctx.strokeStyle = 'green';
                 ctx.lineWidth = 10;
                 ctx.stroke();
+
+                // for test
+                const players = 9;
+                const player_index = 3;
+                const bet_points = 400;
+                const angle = ((2 * Math.PI) * player_index) / players + Math.PI / 2;
+                drawChips(scaledX, scaledY, bet_points, angle, ctx);
             }
         } else {
             console.error("canvas is null", canvas)
@@ -64,22 +71,35 @@ function PokerTable(init_state: ClientState) {
         nextStepHandler(action);
     };
     const nextStepHandler = (type: ActionType) => {
-        console.log(betInputRef.current?.value);
         let value = Number.parseInt(!!betInputRef.current?.value.length ? betInputRef.current?.value : "0");
-        console.log(value);
-            let payload = PlayerPayload.create({ action: { actionType: type, bet: value }, lobbyId: state.lobbyId, playerId: state.playerId });
-            console.log(payload);
+        let payload = PlayerPayload.create({ action: { actionType: type, bet: value }, lobbyId: state.lobbyId, playerId: state.playerId });
 
-            ApiService.sendMessage(payload);
+        ApiService.sendMessage(payload);
 
     }
     return (
-        <div style={{ position: 'relative' }}>
+        <div style={{ position: 'relative', marginLeft: '200px' }}>
             {playerPositions.map((position, index) => (
-                <div key={index} style={{ position: 'absolute', top: position.y, left: position.x, display: 'flex' }}>
-                    {cards.map(({ suit, value }) => {
-                        return <PokerCard cardSuit={index === 0 ? suit : null} cardValue={index === 0 ? value : null} />
-                    })}
+                <div key={index} style={{
+                    position: 'absolute', top: position.y, left: position.x, display: 'flex', flexDirection: 'column',
+                }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', width: "fit-content" }}>
+                        <div style={{ display: 'flex' }}>
+                            {cards.map(({ suit, value }) => {
+                                return <PokerCard cardSuit={index === 0 ? suit : null} cardValue={index === 0 ? value : null} />
+                            })}
+                        </div>
+
+                        <div style={{ alignSelf: 'center', width: "100%", textAlign: "center", backgroundColor: 'wheat', marginTop: '-20px', }}>
+                            <div>
+                                {(state?.players[index]?.bank ?? "100 000") + " chips"}
+                            </div>
+                            <div>
+                                {state?.players[index]?.userName ?? "NickName"}
+                            </div>
+                        </div>
+                    </div>
+
                     {index === 0 ? (
                         <form style={{ display: "flex", alignItems: "flex-end" }}>
                             <input ref={betInputRef} type="number" min="0" max={state?.players.find((p) => p.userId == state.playerId)?.bank || 0}></input>
@@ -109,7 +129,7 @@ function PokerTable(init_state: ClientState) {
                     ) : null}
                 </div>
             ))}
-            <div style={{ position: 'absolute', top: centerY / scaleY, left: centerX / scaleX, display: 'flex' }}>
+            <div style={{ position: 'absolute', top: scaledY, left: scaledX, display: 'flex' }}>
                 {state?.street?.cards.map(({ suit, value }) => {
                     return <PokerCard cardSuit={suit} cardValue={value} />
                 })}
@@ -121,7 +141,51 @@ function PokerTable(init_state: ClientState) {
         </div>
     );
 }
+function drawChips(centerX: number, centerY: number, points: number, angle: number, ctx: CanvasRenderingContext2D) {
+    const x = centerX + Math.cos(angle) * 120;
+    const y = centerY + Math.sin(angle) * 120;
+    // TODO: parse points into digit groups and draw them accordingly 
+    drawChip(x, y, ctx);
+    drawChip(x, y - 3, ctx);
+    drawChip(x, y - 6, ctx);
+    drawChip(x, y - 9, ctx);
+    if (points) {
+        ctx.fillStyle = 'black';
+        ctx.font = '12px Arial';
+        ctx.fillText(`${points} chips`, x, y + 15);
+    }
+}
+function drawChip(x: number, y: number, ctx: CanvasRenderingContext2D, rad: number = 7) {
+    ctx.beginPath();
+    ctx.arc(x, y, rad, 0, Math.PI * 2);
+    ctx.strokeStyle = 'blue';
+    ctx.lineWidth = 1;
+    ctx.fillStyle = 'green';
+    ctx.fill('evenodd');
+    ctx.stroke();
+    ctx.closePath();
+    let numTriangles = 6;
+    let angleStep = (Math.PI * 2) / numTriangles;
 
+    for (let i = 0; i < numTriangles; i++) {
+        ctx.beginPath();
+        let x1 = x + Math.cos(angleStep * i) * rad;
+        let y1 = y + Math.sin(angleStep * i) * rad;
+        ctx.moveTo(x1, y1);
+        let endX = x + Math.cos(angleStep * i) * (rad - 2);
+        let endY = y + Math.sin(angleStep * i) * (rad - 2);
+        ctx.lineTo(endX, endY);
+        ctx.strokeStyle = 'red';
+        ctx.lineWidth = 2;
+        ctx.stroke();
+        ctx.fillStyle = 'black';
+        ctx.font = '10px Arial';
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText("5", x, y);
+        ctx.closePath();
+    }
+}
 function calculatePlayerPositionsFromCanvas(radius: number, xCord: number, yCord: number, scaleX: number, scaleY: number) {
     const numPlayers = 9;
     const positions = [];
@@ -129,9 +193,13 @@ function calculatePlayerPositionsFromCanvas(radius: number, xCord: number, yCord
         const angle = ((2 * Math.PI) * i) / numPlayers + Math.PI / 2;
         const cos_ratio = Math.cos(angle);
         const sin_ratio = Math.sin(angle);
-        const y_fix = sin_ratio < 0 ? -150 : 0;
-
-        const x = xCord + radius * cos_ratio * scaleX;
+        let y_fix = sin_ratio < 0 ? -150 : 0;
+        let x_fix = cos_ratio < 0 ? -180 : 0;
+        // y_fix x_fix depend on card width and height sizes;
+        if (i === 0) {
+            x_fix = -75;
+        }
+        const x = xCord + x_fix + radius * cos_ratio * scaleX;
         const y = yCord + y_fix + radius * sin_ratio * scaleY;
         positions.push({ x, y });
     }
