d0f357219d0f92a9fe9dba85bbe81af00923255e
Rudolf Hrytseniak <rudolf.gritsenyak@gmail.com>
2024-05-10 10:20:59 +0200

    refactor: replace wildcard expansion for proto build files with mapping files names

--- build.rs
+++ build.rs
@@ -1,8 +1,28 @@
 use std::io::Result;
+use std::fs;
+use std::path::Path;
+use prost_build::Config;
 
 fn main() -> Result<()> {
-    let mut config = prost_build::Config::new();
+    let mut config = Config::new();
     config.out_dir("src/protos_rs");
-    config.compile_protos(&["protos/*.proto"], &[""])?;
+
+    let proto_files: Vec<_> = fs::read_dir("protos")?
+        .filter_map(Result::ok)
+        .filter(|entry| {
+            if let Some(extension) = entry.path().extension() {
+                return extension == "proto";
+            }
+            false
+        })
+        .map(|entry| entry.path())
+        .collect();
+
+    let proto_files: Vec<_> = proto_files.iter()
+        .map(|path| path.to_str().unwrap())
+        .collect();
+
+    config.compile_protos(&proto_files, &["protos"])?;
+
     Ok(())
 }
